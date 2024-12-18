08c2b176eb3940e7733e88f2d59aed0528926261
Rugved Wagh <89897353+rugvedwagh@users.noreply.github.com>
2024-12-18 22:53:33 +0530

    Update index.js
    fixed search and infinite loading just changing base url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000'
-    // baseURL: 'https://tempback-1zo9.onrender.com'
+    // baseURL: 'http://localhost:5000'
+    baseURL: 'https://tempback-1zo9.onrender.com'
 })
 
 //  Sending the Token back to our backend for it to verify 
