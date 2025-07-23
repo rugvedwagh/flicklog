d7860d6614f097e588441c4bc33ca73f202a305a
Rugved <rugvedwagh02@gmail.com>
2025-07-23 10:31:44 +0530

    Changed layout of auth page

--- client/src/pages/Auth/Auth.js
+++ client/src/pages/Auth/Auth.js
@@ -1,154 +1,154 @@
-    import { Button, Paper, Grid, Typography, Container, CircularProgress } from '@mui/material';
-    import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-    import { useDispatch, useSelector } from 'react-redux';
-    import { logIn, register } from '../../redux/actions/auth.actions';
-    import { useTheme } from '../../context/themeContext';
-    import React, { useState, useEffect } from 'react';
-    import loginArt from '../../assets/loginart.PNG'
-    import { useNavigate } from 'react-router-dom';
-    import Input from './Input';
-    import './auth.styles.css';
+import { Button, Paper, Grid, Typography, Container, CircularProgress } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { useDispatch, useSelector } from 'react-redux';
+import { logIn, register } from '../../redux/actions/auth.actions';
+import { useTheme } from '../../context/themeContext';
+import React, { useState, useEffect } from 'react';
+import loginArt from '../../assets/loginart.PNG'
+import { useNavigate } from 'react-router-dom';
+import Input from './Input';
+import './auth.styles.css';
 
-    const initialState = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
+const initialState = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+};
+
+const SignUp = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const darkMode = useTheme();
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [isSignup, setIsSignup] = useState(false);
+    const [form, setForm] = useState(initialState);
+
+    const { isLoading } = useSelector((state) => state.authReducer);
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
+
+    const switchMode = () => {
+        setForm(initialState);
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+        setShowPassword(false);
     };
 
-    const SignUp = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        isSignup ? dispatch(register(form, navigate)) : dispatch(logIn(form, navigate));
+    };
 
-        const dispatch = useDispatch();
-        const navigate = useNavigate();
-        const darkMode = useTheme();
+    const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
-        const [showPassword, setShowPassword] = useState(false);
-        const [isSignup, setIsSignup] = useState(false);
-        const [form, setForm] = useState(initialState);
+    return (
+        <Container component="main" maxWidth="lg" style={{ marginTop: '1%', paddingTop: '6rem' }}>
+            <Paper className={`paper-container ${darkMode ? 'dark' : ''}`} elevation={6}>
+                <Grid container spacing={0} alignItems="center">
 
-        const { isLoading } = useSelector((state) => state.authReducer);
-
-        useEffect(() => {
-            window.scrollTo(0, 0);
-        }, []);
-
-        const handleShowPassword = () => {
-            setShowPassword(!showPassword);
-        }
-
-        const switchMode = () => {
-            setForm(initialState);
-            setIsSignup((prevIsSignup) => !prevIsSignup);
-            setShowPassword(false);
-        };
-
-        const handleSubmit = (e) => {
-            e.preventDefault();
-            isSignup ? dispatch(register(form, navigate)) : dispatch(logIn(form, navigate));
-        };
-
-        const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
-
-        return (
-            <Container component="main" maxWidth="md" style={{ marginTop: '1%', paddingTop: '7rem' }}>
-                <Paper className={`paper-container ${darkMode ? 'dark' : ''}`} elevation={6}>
-                    <Grid container spacing={0} alignItems="center">
-
-                        <Grid item xs={12} md={6} className="image-container">
-                            <img src={loginArt} alt="Login Art" className="auth-art" />
-                        </Grid>
-
-                        <Grid item xs={12} md={6}>
-                            <Paper className={`paper ${darkMode ? 'dark' : ''}`} elevation={0} >
-
-                                <form className={`form-auth ${darkMode ? 'dark' : ''}`} onSubmit={handleSubmit}>
-                                    <div className="auth-header">
-                                        <LockOutlinedIcon />
-
-                                        <Typography
-                                            className={`typography ${darkMode ? 'dark' : ''}`}
-                                            component="h1"
-                                            variant="h5"
-                                        >
-                                            {isSignup ? 'Sign up' : 'Log in'}
-                                        </Typography>
-                                    </div>
-
-                                    <Grid container spacing={2}>
-                                        {isSignup && (
-                                            <>
-                                                <Input
-                                                    name="firstName"
-                                                    label="First Name"
-                                                    handleChange={handleChange}
-                                                    autoFocus
-                                                    half
-                                                />
-                                                <Input
-                                                    name="lastName"
-                                                    label="Last Name"
-                                                    handleChange={handleChange}
-                                                    half
-                                                />
-                                            </>
-                                        )}
-                                        <Input
-                                            name="email"
-                                            label="Email Address"
-                                            handleChange={handleChange}
-                                            type="email"
-                                        />
-                                        <Input
-                                            name="password"
-                                            label="Password"
-                                            handleChange={handleChange}
-                                            type={showPassword ? 'text' : 'password'}
-                                            handleShowPassword={handleShowPassword}
-                                        />
-                                        {isSignup && (
-                                            <Input
-                                                name="confirmPassword"
-                                                label="Repeat Password"
-                                                handleChange={handleChange}
-                                                type="password"
-                                            />
-                                        )}
-                                    </Grid>
-
-                                    <Typography variant="body2" className={`subtxt ${darkMode ? 'dark' : ''}`} style={{ marginTop: '8px' }}>
-                                        By logIng up, you agree to our <strong>Terms of Service</strong> and{' '}
-                                        <strong>Privacy Policy</strong>.
-                                    </Typography>
-
-                                    <Button
-                                        type="submit"
-                                        fullWidth
-                                        className={`submit ${darkMode ? 'dark' : ''}`}
-                                        variant="contained"
-                                    >
-                                        {isSignup ? 'Sign Up' : 'Log In'}&nbsp;&nbsp; {isLoading && <CircularProgress size="1.6rem" />}
-                                    </Button>
-
-                                    <Grid style={{ margin: '16px 0' }} container justify="flex-end">
-                                        <Grid item>
-                                            <div
-                                                onClick={switchMode}
-                                                style={{ color: '#2f8ddf', cursor: 'pointer' }}
-                                            >
-                                                {isSignup
-                                                    ? 'Already have an account? Log in'
-                                                    : "Don't have an account? Sign Up"}
-                                            </div>
-                                        </Grid>
-                                    </Grid>
-                                </form>
-                            </Paper>
-                        </Grid>
+                    <Grid item xs={12} md={6} className="image-container">
+                        <img src={loginArt} alt="Login Art" className="auth-art" />
                     </Grid>
-                </Paper>
-            </Container >
-        );
-    };
 
-    export default SignUp;
+                    <Grid item xs={12} md={6}>
+                        <Paper className={`paper ${darkMode ? 'dark' : ''}`} elevation={0} >
+
+                            <form className={`form-auth ${darkMode ? 'dark' : ''}`} onSubmit={handleSubmit}>
+                                <div className="auth-header">
+                                    <LockOutlinedIcon />
+
+                                    <Typography
+                                        className={`typography ${darkMode ? 'dark' : ''}`}
+                                        component="h1"
+                                        variant="h5"
+                                    >
+                                        {isSignup ? 'Sign up' : 'Log in'}
+                                    </Typography>
+                                </div>
+
+                                <Grid container spacing={2}>
+                                    {isSignup && (
+                                        <>
+                                            <Input
+                                                name="firstName"
+                                                label="First Name"
+                                                handleChange={handleChange}
+                                                autoFocus
+                                                half
+                                            />
+                                            <Input
+                                                name="lastName"
+                                                label="Last Name"
+                                                handleChange={handleChange}
+                                                half
+                                            />
+                                        </>
+                                    )}
+                                    <Input
+                                        name="email"
+                                        label="Email Address"
+                                        handleChange={handleChange}
+                                        type="email"
+                                    />
+                                    <Input
+                                        name="password"
+                                        label="Password"
+                                        handleChange={handleChange}
+                                        type={showPassword ? 'text' : 'password'}
+                                        handleShowPassword={handleShowPassword}
+                                    />
+                                    {isSignup && (
+                                        <Input
+                                            name="confirmPassword"
+                                            label="Repeat Password"
+                                            handleChange={handleChange}
+                                            type="password"
+                                        />
+                                    )}
+                                </Grid>
+
+                                <Typography variant="body2" className={`subtxt ${darkMode ? 'dark' : ''}`} style={{ marginTop: '8px' }}>
+                                    By logIng up, you agree to our <strong>Terms of Service</strong> and{' '}
+                                    <strong>Privacy Policy</strong>.
+                                </Typography>
+
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    className={`submit ${darkMode ? 'dark' : ''}`}
+                                    variant="contained"
+                                >
+                                    {isSignup ? 'Sign Up' : 'Log In'}&nbsp;&nbsp; {isLoading && <CircularProgress size="1.6rem" />}
+                                </Button>
+
+                                <Grid style={{ margin: '16px 0' }} container justify="flex-end">
+                                    <Grid item>
+                                        <div
+                                            onClick={switchMode}
+                                            style={{ color: '#2f8ddf', cursor: 'pointer' }}
+                                        >
+                                            {isSignup
+                                                ? 'Already have an account? Log in'
+                                                : "Don't have an account? Sign Up"}
+                                        </div>
+                                    </Grid>
+                                </Grid>
+                            </form>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Paper>
+        </Container >
+    );
+};
+
+export default SignUp;
